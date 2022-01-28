34bf48199749b73d44dcae5b8ebd59e2ab00e7e8
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2022-01-28 14:32:53 +0900

    learned how to use git

--- OpenfaceExtract.py
+++ OpenfaceExtract.py
@@ -1,7 +1,24 @@
+# -*- coding: utf-8 -*-
 #OpenFaceを用いてビデオデータから特徴量を抽出する
 import glob
 import os
 
-path = './loggerstation/'
+default_path = '/tmp/loggerstation'
+# default_path = './loggerstation'
+users = ['vp007', 'vp008', 'vp009', 'vp010']
 
-os.chdir(path)
+############################################################################
+make_cmd = '/home/openface-build/build/make'
+# print(make_cmd)
+os.system(make_cmd)
+
+for user in users:
+    date_dirs = os.listdir(default_path + '/' + user)
+    for date_dir in date_dirs:
+        video_path = os.path.join(default_path, user, date_dir, 'camera_raw.mp4') 
+        out_path = os.path.join(default_path, user, date_dir)
+        
+        run_cmd = "/home/openface-build/build/bin/FeatureExtraction -f " + video_path + " -out_dir " + out_path + "/FaceFeature"
+        print(run_cmd)
+        os.system(run_cmd)
+        
